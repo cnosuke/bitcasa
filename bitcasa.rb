d57b5e9849fa5a4de65230bf4f6fed76b2848ba6
Shinnosuke Takeda <shinnosuke@gmail.com>
2014-04-20 00:35:49 +0900

    Add base function with rest-client

--- bitcasa.rb
+++ bitcasa.rb
@@ -5,12 +5,23 @@ module Bitcasa
   VERSION = '0.0.1'
 
   class RESTClient
+    API_BASE_URL = 'https://developer.api.bitcasa.com/v1'.freeze
+
     def initialize(access_token: nil)
       @access_token = access_token || ENV['ACCESS_TOKEN'] || raise(ArgumentError)
     end
 
     def access_token
       @access_token
+    end
+
+    def api(method, path, resource = nil, opts = {})
+      url = [API_BASE_URL, path, resource].join('/') + "?access_token=#{access_token}"
+      response = RestClient.send(method, url, opts)
+      {
+        response: response,
+        body: JSON.parse(response)
+      }
     end
   end
 end
